e21325e78a8495ee42199dafda5f5c3b17e40df4
springveil27 <springveil27@gmail.com>
2025-02-19 17:03:38 -0400

    Tarea 4

---
+++ Tarea_4/CompleteCalculatorClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+public class Calculadora
+{
+    private double ultimoResultado;
+    public double UltimoResultado
+    {
+        get { return ultimoResultado; }
+        private set { ultimoResultado = value; }
+    }
+
+    public double Sumar(double a, double b)
+    {
+        UltimoResultado = a + b;
+        return UltimoResultado;
+    }
+    public double Restar(double a, double b)
+    {
+        ultimoResultado = a - b;
+        return ultimoResultado;
+    }
+    public double Multiplicar(double a, double b)
+    {
+        ultimoResultado = a * b;
+        return ultimoResultado;
+    }
+    public double Dividir(double a, double b)
+    {
+        try
+        {
+            ultimoResultado = a / b;
+
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+            UltimoResultado = double.NaN;
+        };
+        return ultimoResultado;
+    }
+}
